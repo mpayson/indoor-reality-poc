679cd09401676141ba6f1bbe77ebf11c59f26fe0
Max Payson <mpayson@esri.com>
2017-07-05 11:23:47 -0700

    first commit

---
+++ layerconfig.js
@@ -0,0 +1,26 @@
+const layerConfig = {
+  "Ceilings": {
+    opcRange: [750, 350],
+    hasFloorAtr: true
+  },
+  "Windows": {
+    opcRange: [750, 350],
+    hasFloorAtr: false,
+  },
+  "Outdoor Walls":{
+    opcRange: [750, 350],
+    hasFloorAtr: false
+  },
+  "Doors": {
+    opcRange: [300, -50],
+    hasFloorAtr: true
+  },
+  "Floors": {
+    opcRange: [300, -50],
+    hasFloorAtr: true
+  },
+  "Indoor Walls": {
+    opcRange: [300, -50],
+    hasFloorAtr: true
+  }
+}
